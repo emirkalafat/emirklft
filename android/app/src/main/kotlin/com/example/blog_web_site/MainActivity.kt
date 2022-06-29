6f2ddc3dfa187be1198dc256efcb108de512eb30
Emir Kalafat <emirklft@outlook.com>
2022-06-29 21:43:02 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/blog_web_site/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blog_web_site
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print("error");
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return const MyHomePage();
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ahmet Emir Kalafat"),
+      ),
+      body: const Center(
+        child: Text(
+          "Merhaba",
+        ),
+      ),
+    );
+  }
+}
