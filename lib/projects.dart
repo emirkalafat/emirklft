86d855e69c33b1667b139248eb1dee4bd42c3319
Emir Kalafat <emirklft@outlook.com>
2022-09-08 16:09:31 +0300

    secondary

---
+++ lib/projects.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const List<Map<String, dynamic>> projects = [
+  {
+    'id': '1',
+    'title': 'Enfes Tarifler',
+    'url': '',
+  },
+  //{},
+  //{}
+];
