5260b922e24cb1371d52a76dc53307f61284bc8b
Emir Kalafat <emirklft@outlook.com>
2022-10-12 17:12:46 +0300

    blog

--- lib/data/blog_post_model.dart
+++ lib/data/blog_post_model.dart
@@ -2,13 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BlogPost {
   String title;
-  String content;
+  List<dynamic> contents;
   Timestamp date;
   String? category;
   String id;
   BlogPost({
     required this.title,
-    required this.content,
+    required this.contents,
     required this.date,
     required this.category,
     this.id = '',
@@ -27,7 +27,7 @@ class BlogPost {
 
   static BlogPost fromJson(Map<String, dynamic> json) => BlogPost(
         category: json['category'] ?? 'Havadan Sudan',
-        content: json['content'],
+        contents: json['contents'],
         title: json['title'],
         date: json['date'],
       );
--- lib/screens/blog.dart
+++ lib/screens/blog.dart
@@ -1,4 +1,3 @@
-import 'package:beamer/beamer.dart';
 import 'package:blog_web_site/data/blog_post_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -36,61 +35,9 @@ class MyBlog extends StatelessWidget {
                 child: ListView.builder(
                   itemCount: blogPosts.length,
                   itemBuilder: (context, index) {
-                    final yayinTarihi = blogPosts[index].date.toDate();
-                    final yayinTarihiFarki =
-                        Timestamp.now().toDate().difference(yayinTarihi);
-
-                    return Container(
-                      constraints: const BoxConstraints(
-                        maxWidth: 1000,
-                        minHeight: 200,
-                      ),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 12.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    blogPosts[index].title.toUpperCase(),
-                                    style: const TextStyle(fontSize: 24),
-                                  ),
-                                  const Spacer(),
-                                  Text(yayinTarihiFarki.inHours > 24
-                                      ? "${yayinTarihiFarki.inDays} gün önce"
-                                      : yayinTarihiFarki.inMinutes > 60
-                                          ? "${yayinTarihiFarki.inHours} saat önce"
-                                          : "${yayinTarihiFarki.inMinutes} dakika önce"),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                      " Yayınlanma Tarihi: ${yayinTarihi.day} ${yayinTarihi.month} ${yayinTarihi.year} ${days[yayinTarihi.weekday - 1]}  ${yayinTarihi.hour}:${yayinTarihi.minute}"),
-                                ],
-                              ),
-                              Divider(),
-                              Text(
-                                blogPosts[index].content,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
-                                maxLines: 4,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              InkWell(
-                                  onTap: () {
-                                    Beamer.of(context).beamToNamed(
-                                        "/blog/${blogPosts[index].id}");
-                                  },
-                                  child: Text("Devamını Okuyayım...")),
-                            ],
-                          ),
-                        ),
-                      ),
+                    return BlogPostWidget(
+                      blogPosts: blogPosts,
+                      index: index,
                     );
                   },
                 ),
@@ -100,6 +47,111 @@ class MyBlog extends StatelessWidget {
         }
         return const Center(child: CircularProgressIndicator());
       },
+    );
+  }
+}
+
+class BlogPostWidget extends StatefulWidget {
+  const BlogPostWidget({
+    Key? key,
+    required this.blogPosts,
+    required this.index,
+  }) : super(key: key);
+
+  final List<BlogPost> blogPosts;
+  final int index;
+
+  @override
+  State<BlogPostWidget> createState() => _BlogPostWidgetState();
+}
+
+class _BlogPostWidgetState extends State<BlogPostWidget> {
+  bool isExpanded = false;
+  @override
+  Widget build(BuildContext context) {
+    final yayinTarihi = widget.blogPosts[widget.index].date.toDate();
+    final yayinTarihiFarki = Timestamp.now().toDate().difference(yayinTarihi);
+    List<String> eachContent = (widget.blogPosts[widget.index].contents)
+        .map((item) => item as String)
+        .toList();
+
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 1000,
+        minHeight: 200,
+      ),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(
+                  widget.blogPosts[widget.index].title.toUpperCase(),
+                  style: const TextStyle(fontSize: 24),
+                ),
+              ),
+              const Divider(),
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Container(
+                  constraints: BoxConstraints(
+                      maxHeight: isExpanded ? double.infinity : 180),
+                  child: ScrollConfiguration(
+                    behavior: ScrollConfiguration.of(context)
+                        .copyWith(scrollbars: false),
+                    //listview kaydırılmasın diye
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: eachContent.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, contentIndex) {
+                        final text = eachContent[contentIndex];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(text),
+                            const SizedBox(
+                              height: 8,
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isExpanded = !isExpanded;
+                      });
+                    },
+                    child: Text(isExpanded ? 'Küçült' : 'Devamını Oku'),
+                  ),
+                  const Spacer(),
+                  Text(yayinTarihiFarki.inHours > 24
+                      ? "${yayinTarihiFarki.inDays} gün önce"
+                      : yayinTarihiFarki.inMinutes > 60
+                          ? "${yayinTarihiFarki.inHours} saat önce"
+                          : "${yayinTarihiFarki.inMinutes} dakika önce  "),
+                  Text(
+                      "  |  ${yayinTarihi.day}-${yayinTarihi.month}-${yayinTarihi.year} ${days[yayinTarihi.weekday - 1]}  ${yayinTarihi.hour}:${yayinTarihi.minute.toString().padLeft(2, '0')}"),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
