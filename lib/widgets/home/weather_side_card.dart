6a902ad0f4aebc823808636055b9e2aa5e922edd
emirkalafat <emirklft@outlook.com>
2023-07-25 12:02:45 +0300

    weather request fix

--- lib/widgets/home/weather_side_card.dart
+++ lib/widgets/home/weather_side_card.dart
@@ -31,12 +31,13 @@ class _WeatherSideCardState extends State<WeatherSideCard> {
     wf = WeatherFactory("8afeaa7003fbb9940622882daf57b218",
         language: Language.TURKISH);
     w = await wf.currentWeatherByLocation(
-        konum?.latitude ?? 41.0111, konum?.altitude ?? 28.9711);
-    return 0;
+        konum?.latitude ?? 41.0111, konum?.longitude ?? 28.9711);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    final bodySmall = Theme.of(context).textTheme.bodySmall;
     return Card(
       color: Theme.of(context).colorScheme.secondaryContainer,
       margin: const EdgeInsets.all(8),
@@ -44,45 +45,70 @@ class _WeatherSideCardState extends State<WeatherSideCard> {
         padding: const EdgeInsets.fromLTRB(16, 16, 16, 48),
         child: Column(
           mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-                'https://openweathermap.org/img/wn/${w?.weatherIcon ?? '01d'}@2x.png'),
-            const SizedBox(height: 8),
-            RichText(
-              text: TextSpan(
-                  style: Theme.of(context).textTheme.headlineMedium,
-                  text: w != null
-                      ? isCelcius
-                          ? '${w?.temperature!.celsius!.round()} '
-                          : '${w?.temperature!.fahrenheit!.round()} '
-                      : '',
-                  children: [
-                    TextSpan(
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium!
-                            .copyWith(decoration: TextDecoration.underline),
-                        text: isCelcius ? '°C' : '°F',
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            setState(() {
-                              isCelcius = !isCelcius;
-                            });
-                          })
-                  ]),
-            ),
-            const SizedBox(height: 8),
-            Text(w?.weatherDescription!.toTitleCase() ?? ''),
-            const SizedBox(height: 8),
-            Text(w?.areaName ?? ''),
-            const SizedBox(height: 8),
-            Text(w?.country ?? ''),
-            const SizedBox(height: 8),
-            Text('${w?.humidity ?? ''}'),
-            const SizedBox(height: 8),
-            Text('${w?.windSpeed ?? ''}'),
-            const SizedBox(height: 8),
-            Text('${w?.windDegree ?? ''}'),
+            if (w == null) ...[
+              const Icon(Icons.location_disabled),
+              const SizedBox(height: 8),
+              const Text(
+                'Konum Bilgisi Alınamadı',
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    await Geolocator.requestPermission();
+                    getLocation();
+                    setState(() {});
+                  },
+                  child: const Text('Tekrar Dene')),
+            ],
+            if (w != null) ...[
+              Image.network(
+                  'https://openweathermap.org/img/wn/${w?.weatherIcon ?? '01d'}@2x.png'),
+              const SizedBox(height: 8),
+              RichText(
+                text: TextSpan(
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    text: w != null
+                        ? isCelcius
+                            ? '${w?.temperature!.celsius!.round()} '
+                            : '${w?.temperature!.fahrenheit!.round()} '
+                        : '',
+                    children: [
+                      TextSpan(
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium!
+                              .copyWith(decoration: TextDecoration.underline),
+                          text: isCelcius ? '°C' : '°F',
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              setState(() {
+                                isCelcius = !isCelcius;
+                              });
+                            })
+                    ]),
+              ),
+              const SizedBox(height: 8),
+              Text('Hava Koşulu: ${w!.weatherDescription!.toTitleCase()}'),
+              const SizedBox(height: 8),
+              Text('Konum: ${w?.areaName ?? ''} ${w?.country ?? ''}'),
+              const SizedBox(height: 8),
+              Text(
+                'Nem Oranı: ${w?.humidity ?? ''}',
+                style: bodySmall,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Rüzgar Hızı: ${w?.windSpeed ?? ''} m/s',
+                style: bodySmall,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Rüzgar Açısı: ${w?.windDegree ?? ''}',
+                style: bodySmall,
+              ),
+            ]
           ],
         ),
       ),
